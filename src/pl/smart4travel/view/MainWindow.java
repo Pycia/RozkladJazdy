15e1e03d51d55d1a30d74240dbfc6cdc0aa6ec67
janiec <mjjaniec@gmail.com>
2013-12-30 22:49:52 +0100

    Dodanie nowego okna Okno + logo

--- src/pl/smart4travel/view/MainWindow.java
+++ src/pl/smart4travel/view/MainWindow.java
@@ -1,11 +1,36 @@
 package pl.smart4travel.view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 /**
  * Created by Natalia on 30/12/13.
  */
 public class MainWindow extends JFrame {
+    public MainWindow() throws HeadlessException {
+        getRootPane().setLayout(new BoxLayout(getRootPane(),BoxLayout.Y_AXIS));
+        try
+        {
+            // Read from a file
+            File FileToRead;
+            FileToRead = new File("resources\\images\\logo.png");
+            //Recognize file as image
+            Image Picture = ImageIO.read(FileToRead);
+            //Show the image inside the label
+            JLabel jLabelImage = new JLabel();
+            jLabelImage.setIcon(new ImageIcon(Picture));
+            getRootPane().add(jLabelImage);
+        }
+        catch (Exception e)
+        {
+            //Display a message if something goes wrong
+            JOptionPane.showMessageDialog( null, e.toString() );
+        }
+
+    }
+
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
